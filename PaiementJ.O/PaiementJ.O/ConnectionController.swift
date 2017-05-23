803d91fca77d1d52f5c51ecd6860408e75b6bc4c
joffreypijoan <joffrey.pijoan@imerir.com>
2017-05-23 14:19:27 +0200

    App finish with request

--- PaiementJ.O/PaiementJ.O/ConnectionController.swift
+++ PaiementJ.O/PaiementJ.O/ConnectionController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 
 class ConnectionController: UIViewController {
+    
+    let url_request = "http://172.30.0.120:5000/user/account"
 
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -24,10 +26,42 @@ class ConnectionController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func clickedConnection(_ sender: Any) {
-        //if compte bon
-        performSegue(withIdentifier: "segueToScan", sender: nil)
-        //else
-        //alertview (login ou password mauvais)
+        
+        if (passwordTextField.text! == "" || loginTextField.text! == "") {
+            display_alert_with_one_action(title: "Erreur formulaire", message: "Veuillez remplir le formulaire pour vous connectez", titleAction: "Fermer")
+        }
+        else {
+            let email_tf_text = loginTextField.text!
+            let password_tf_text = passwordTextField.text!
+        
+            //On regarde si l'utilisateur existe dans la base de données
+            let result_request = RequestManager.do_post_request_account(atUrl: url_request, withData: ["mail":email_tf_text, "motdepasse":password_tf_text])
+            print(result_request)
+        
+            //L'utilisateur n'existe pas dans la base de données
+            if (result_request.count == 0) {
+                display_alert_with_one_action(title: "Champs incorrects", message: "Email ou mot de passe incorrects", titleAction: "Fermer")
+            }
+        
+            //L'utilisateur existe dans la base de données
+            if (result_request.count > 0) {
+                let result_type_request = result_request[0]["type"] as! String
+                if (result_type_request == "orga") {
+                    performSegue(withIdentifier: "segueToScan", sender: nil)
+                }
+                else {
+                    display_alert_with_one_action(title: "Erreur compte", message: "Seule les compte organisateurs peuvent accedez au service", titleAction: "Fermer")
+                }
+            }
+        }
     }
+    
+    internal func display_alert_with_one_action (title:String?, message:String?, titleAction:String?) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let done = UIAlertAction(title: titleAction, style: .default, handler: nil)
+        alertVC.addAction(done)
+        self.present(alertVC, animated: true, completion: nil)
+    }
+
 
 }
--- PaiementJ.O/PaiementJ.O/RequestManager.swift
+++ PaiementJ.O/PaiementJ.O/RequestManager.swift
@@ -17,4 +17,49 @@ class RequestManager: NSObject {
         let datas = JSON(content! as Data).arrayObject as! [[String:Any]]
         return datas
     }
+    
+    @discardableResult
+    static func do_post_request_account (atUrl:String, withData:[String:Any]) -> [[String:Any]] {
+        var result_request : [[String:Any]]?
+        
+        DispatchQueue.global(qos: .userInitiated).sync {
+            let group = DispatchGroup()
+            group.enter()
+            
+            //Debut de la requete
+            let requestObject = NSMutableURLRequest(url: URL(string: atUrl)!)
+            requestObject.httpMethod = "POST"
+            requestObject.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            requestObject.addValue("application/json", forHTTPHeaderField: "Accept")
+            
+            let posting = JSON(withData)
+            requestObject.httpBody = posting.rawString()!.data(using: String.Encoding.utf8)
+            print(posting.rawString()!.data(using: String.Encoding.utf8)!)
+            
+            URLSession.shared.dataTask(with: (requestObject as URLRequest), completionHandler: { (data, response, error) in
+                
+                if (error != nil) {
+                    print(error!.localizedDescription)
+                    result_request = [[String:Any]]()
+                    group.leave()
+                }
+                else {
+                    let httpcode = (response as? HTTPURLResponse)?.statusCode
+                    if (httpcode == 200) {
+                        let response_of_server = JSON(data!)
+                        result_request = response_of_server.arrayObject as? [[String:Any]]
+                        group.leave()
+                    }
+                    else {
+                        print("passage dans le 204, no content")
+                        result_request = [[String:Any]]()
+                        group.leave()
+                    }
+                }
+            }).resume()
+            group.wait()
+        }
+        return result_request!
+    }
+
 }
